f2a6281cbf94b4d975efc745f618c5fcf856f4d4
DeLamDev <hdelamadrid@protonmail.com>
2023-02-09 23:11:06 -0600

    Add missing challenge chapter 3

---
+++ chapter_3/christmas.rs
@@ -0,0 +1,15 @@
+
+
+
+fn main() {
+    let days = ["first", "second", "third", "fourth", "fifth", "sixth",
+    "nineth", "tenth", "eleventh", "twelfth"];
+    
+    let chorus = "On the {} day of Christmas my true love sent to me";
+
+
+
+    for day in days {
+
+    }
+}
