9e580589085ccab9c78970f64ae27a37323fb24f
DeLamDev <hdelamadrid@protonmail.com>
2023-02-15 21:58:48 -0600

    Modified christmas.rs

--- chapter_3/christmas.rs
+++ chapter_3/christmas.rs
@@ -7,9 +7,17 @@ fn main() {
     
     let chorus = "On the {} day of Christmas my true love sent to me";
 
+    let numbers = ["", "Two", "Three", "Four", "Five", "Six", "Seven",
+    "Eight", "Nine", "Ten", "Eleven", "Twelve"];
 
+    let gifts = ["{}A patridge in a pear tree", "{} turtle doves, and",
+    "{} french hens", "{} calling birds", "{} golden rings", "{} geese a-laying",
+    "{} swans a-swimming", "{} maids a-milking", "{} ladies dancing",
+    "{} lords a-leaping", "{} pipers piping", "{} drummers drumming"];
+    
+    let mut counter: u8 = 0; 
 
     for day in days {
-
+        println!(chorus, day);
     }
 }
